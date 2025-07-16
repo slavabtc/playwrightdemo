01baab9cf86e5efaf4e4c0836a50345a5df81ebf
slavabtc <garilyuk.igor322@gmail.com>
2025-07-16 22:03:42 +0200

    - added second test for login + added explicit waits ( 1 second , just to visualise the flow in browser)

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,4 +1,5 @@
 import pytest
+import time
 from pages.login_page import LoginPage
 
 @pytest.mark.parametrize("username,password", [
@@ -11,3 +12,15 @@ def test_valid_login(page, username, password):
 
 
     assert page.url == "https://www.saucedemo.com/inventory.html"
+
+def test_locked_out_user_login(page):
+    login = LoginPage(page)
+    login.load()
+    time.sleep(1)
+    login.login("locked_out_user", "secret_sauce")
+    time.sleep(1)
+
+    # Assert error message is visible
+    assert login.error_message.is_visible()
+    assert "locked out" in login.error_message.text_content().lower()
+    time.sleep(1)
